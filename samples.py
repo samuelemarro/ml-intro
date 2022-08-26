ea26a1e373998bcc68013274049da8d5010d8b27
Samuele Marro <marrosamuele@gmail.com>
2022-08-26 10:22:31 +0200

    Initial upload.

---
+++ samples.py
@@ -0,0 +1,20 @@
+from PIL import Image
+import os
+from pathlib import Path
+
+import numpy as np
+
+def load_sample(name, rescale=True, crop=True):
+    base_path = os.path.abspath(os.path.dirname(__file__))
+    path = Path(base_path) / 'img' / (name + ('_crop' if crop else '') + '.png')
+    image = Image.open(path)
+    image = np.asarray(image)
+
+    if rescale:
+        image = image.astype(np.float) / 255
+    return image
+
+sample_names = ['secretary']
+
+for sample_name in sample_names:
+    globals()[sample_name] = load_sample(sample_name)
---
+++ utils.py
@@ -0,0 +1,30 @@
+from math import ceil
+import matplotlib.pyplot as plt
+
+def normalize_image(image):
+    pass
+
+def plot_image():
+    pass
+
+def plot_images(images, columns=2, titles=None):
+    columns = min(columns, len(images))
+    rows = int(ceil(len(images) / columns))
+
+    if columns == 1 and rows == 1:
+        plt.imshow(images[0])
+        if titles is not None:
+            plt.title(titles[0])
+        plt.axis('off')
+    else:
+        fig, axs = plt.subplots(rows, columns, figsize=(5 * columns, 5 * rows))
+        axs = axs.flatten()
+
+        for i in range(len(images)):
+            axs[i].imshow(images[i])
+            if titles is not None:
+                axs[i].set_title(titles[i])
+        for axis in axs:
+            axis.axis('off')
+    plt.show()
+    #plt.show()
