4c13d6cb25ad5b17654186c9ee55bdabd95d7a18
Samuele Marro <marrosamuele@gmail.com>
2022-11-08 20:32:25 +0100

    Fixed flowers.

--- data.py
+++ data.py
@@ -7,7 +7,9 @@ FLOWERS_URL = 'https://github.com/samuelemarro/ml_intro/releases/download/untagg
 
 def flowers_dataset():
     base_path = os.path.abspath(os.path.dirname(__file__))
-    path = Path(base_path) / 'data' / 'flowers.npy'
-    images, labels = np.load(path.as_posix(), allow_pickle=True)
+    path_images = Path(base_path) / 'data' / 'flowers_images.npy'
+    images = np.load(path_images.as_posix(), allow_pickle=True)
+    path_labels = Path(base_path) / 'data' / 'flowers_labels.npy'
+    labels = np.load(path_labels.as_posix(), allow_pickle=True)
     return images, labels
     
