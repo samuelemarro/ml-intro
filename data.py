bc0ee99cfac86b809843e275c395bb15cb0865a3
Samuele Marro <marrosamuele@gmail.com>
2022-11-21 15:30:58 +0100

    Added papaya_pumpkin dataset.

--- data.py
+++ data.py
@@ -3,13 +3,18 @@ from pathlib import Path
 
 import numpy as np
 
-FLOWERS_URL = 'https://github.com/samuelemarro/ml_intro/releases/download/untagged-1b15fbe0781533e6a82c/flowers.npy'
+def _load_dataset(name):
+    base_path = os.path.abspath(os.path.dirname(__file__))
+    path = Path(base_path) / 'data' / name
+    return np.load(path.as_posix(), allow_pickle=True)
+
 
 def flowers_dataset():
-    base_path = os.path.abspath(os.path.dirname(__file__))
-    path_images = Path(base_path) / 'data' / 'flowers_images.npy'
-    images = np.load(path_images.as_posix(), allow_pickle=True)
-    path_labels = Path(base_path) / 'data' / 'flowers_labels.npy'
-    labels = np.load(path_labels.as_posix(), allow_pickle=True)
+    images = _load_dataset('flowers_images.npy')
+    labels = _load_dataset('flowers_labels.npy')
     return images, labels
-    
+
+def papaya_pumpkin_dataset():
+    images = _load_dataset('papaya_pumpkin_images.npy')
+    labels = _load_dataset('papaya_pumpkin_labels.npy')
+    return images, labels
