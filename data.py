cca28753d3e5bbedd395a4e9b9dd8c1d939593dd
Samuele Marro <marrosamuele@gmail.com>
2022-11-21 16:51:30 +0100

    Changed file management.

--- data.py
+++ data.py
@@ -3,18 +3,16 @@ from pathlib import Path
 
 import numpy as np
 
-def _load_dataset(name):
+def load_dataset_file(path):
+    return np.load(path.as_posix(), allow_pickle=True)
+    
+def _load_internal_dataset(name):
     base_path = os.path.abspath(os.path.dirname(__file__))
     path = Path(base_path) / 'data' / name
-    return np.load(path.as_posix(), allow_pickle=True)
+    return load_dataset_file(path)
 
 
 def flowers_dataset():
-    images = _load_dataset('flowers_images.npy')
-    labels = _load_dataset('flowers_labels.npy')
-    return images, labels
-
-def papaya_pumpkin_dataset():
-    images = _load_dataset('papaya_pumpkin_images.npy')
-    labels = _load_dataset('papaya_pumpkin_labels.npy')
+    images = _load_internal_dataset('flowers_images.npy')
+    labels = _load_internal_dataset('flowers_labels.npy')
     return images, labels
