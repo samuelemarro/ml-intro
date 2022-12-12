28a8aa93d7c7068025c9a875c3ff5c800716ffa7
Samuele Marro <marrosamuele@gmail.com>
2022-12-12 02:27:05 +0100

    Added headlines dataset.

--- data.py
+++ data.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import json
 
 import numpy as np
+import pandas as pd
 
 def load_dataset_file(path):
     if isinstance(path, Path):
@@ -32,3 +33,8 @@ def sp250_dataset():
     with open('data/sp250_info.json') as f:
         data = json.load(f)
     return _load_internal_dataset('sp250_values.npy'), np.array(data['category']), data['names'], data['category_names']
+
+def headlines_dataset():
+    data = pd.read_csv('data/headlines.csv')
+
+    return list(data['headlines']), list(data['complete_text'])
